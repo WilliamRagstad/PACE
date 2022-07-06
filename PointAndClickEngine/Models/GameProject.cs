1ddfbdb4f7dabfb132a45e1dad65cd421793d162
William Rågstad <william.ragstad@gmail.com>
2022-07-06 23:02:16 +0200

    Move serialization to its own file

--- PointAndClickEngine/Models/GameProject.cs
+++ PointAndClickEngine/Models/GameProject.cs
@@ -4,23 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace PointAndClickEngine.Models
 {
+	[Serializable]
 	public class GameProject
 	{
 		public string Title { get; set; }
 		public string Description { get; set; }
-
-		public static string ProjectRootFile = "project.pace";
-		public static GameProject LoadProjectFolder(string folderPath) =>
-			LoadFile(Path.Combine(folderPath, ProjectRootFile));
-		public static GameProject LoadFile(string projectRootPath)
-		{
-			GameProject project = new GameProject();
-			project.Title = projectRootPath;
-			project.Description = projectRootPath;
-			return project;
-		}
 	}
 }
---
+++ PointAndClickEngine/Models/Serialization/GameProjectSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PointAndClickEngine.Models.Serialization
+{
+	public class GameProjectSerializer
+	{
+		public static string ProjectRootFile = "project.pace";
+		private static string ProjectRootFromFolder(string folderPath) =>
+			Path.Combine(folderPath, ProjectRootFile);
+
+		private static readonly XmlSerializer _serializer = new XmlSerializer(typeof(GameProject));
+
+		public static GameProject LoadProjectFolder(string folderPath) =>
+			LoadFile(ProjectRootFromFolder(folderPath));
+		public static GameProject LoadFile(string projectRootPath)
+		{
+			FileStream fs = File.OpenRead(projectRootPath);
+			GameProject project = (GameProject)_serializer.Deserialize(fs);
+			if (project == null) throw new Exception($"Failed to load project: {projectRootPath}!");
+			return project;
+		}
+
+		/// <summary>
+		/// Save PACE game project in root of folder.
+		/// </summary>
+		public static void SaveToFolder(GameProject project, string folderPath)
+		{
+			var root = ProjectRootFromFolder(folderPath);
+			if (File.Exists(root)) File.Delete(root);
+			FileStream fs = File.Create(root);
+			_serializer.Serialize(fs, project);
+			fs.Close();
+		}
+	}
+}
