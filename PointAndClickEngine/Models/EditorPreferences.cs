0cfbcd8e4071e6a8546b66618f09a4c75f1c16ea
William Rågstad <william.ragstad@gmail.com>
2022-07-07 19:28:00 +0200

    Update EditorPreferences.cs

--- PointAndClickEngine/Models/EditorPreferences.cs
+++ PointAndClickEngine/Models/EditorPreferences.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace PointAndClickEngine.Models
 {
@@ -11,11 +12,17 @@ namespace PointAndClickEngine.Models
 	/// Intended to be saved and loaded from file in appdata.
 	/// </summary>
 	[Serializable]
+	[XmlRoot("Preferences", IsNullable = false)]
 	public class EditorPreferences
 	{
+		[XmlElement("Language")]
+		public string Language = "sv";
 		/// <summary>
 		/// Current editor theme.
 		/// </summary>
+		[XmlElement("Theme", IsNullable = false)]
 		public string ThemeName = nameof(ThemeManager.Builtins.Default);
+		[XmlElement("EditorCommand", IsNullable = false)]
+		public string StartCodeEditorCommand = "code $file"; // VSCode
 	}
 }
