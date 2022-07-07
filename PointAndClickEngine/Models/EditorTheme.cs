c5598b13a8cc7ec0dc110e7df843b1f68d6e6f87
William Rågstad <william.ragstad@gmail.com>
2022-07-07 12:46:01 +0200

    Added initial editor theme

---
+++ PointAndClickEngine/Models/EditorTheme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointAndClickEngine.Models
+{
+	[Serializable]
+	public class EditorTheme
+	{
+		public Color FormBackgroundPrimary;
+		public Color FormTextPrimary;
+		public Color PaceButtonPrimary;
+	}
+}
