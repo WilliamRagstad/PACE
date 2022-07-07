6fb7ef0d36f97d7df19154485caab976cae6c882
William Rågstad <william.ragstad@gmail.com>
2022-07-07 20:19:18 +0200

    Add list view ext

---
+++ PACE_Controls/ListViewExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PACE_Controls
+{
+	public partial class ListViewExt : ListView
+	{
+		public ListViewExt()
+		{
+			InitializeComponent();
+		}
+
+		public ListViewExt(IContainer container)
+		{
+			container.Add(this);
+
+			InitializeComponent();
+		}
+	}
+}
