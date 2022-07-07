401cb79a876a90ef67e08e0a6d72ebfebeb4e7e4
William Rågstad <william.ragstad@gmail.com>
2022-07-07 13:21:47 +0200

    Move Editor Preferences

---
+++ PointAndClickEngine/Models/EditorPreferences.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointAndClickEngine.Models
+{
+	[Serializable]
+	public class EditorPreferences
+	{
+		public EditorTheme Theme;
+	}
+}
