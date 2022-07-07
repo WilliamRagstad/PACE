d6dd019f235f77146986fccc7983709b6ef7083c
William Rågstad <william.ragstad@gmail.com>
2022-07-07 13:22:06 +0200

    Load preferences on start

--- PointAndClickEngine/EngineConfig.cs
+++ PointAndClickEngine/EngineConfig.cs
@@ -12,9 +12,28 @@ namespace PointAndClickEngine
 	internal static class EngineConfig
 	{
 		public static string Version = "1.0.0-Alpha";
-		public static string ProjectRootFile = "project.pace";
+		public static EditorPreferences EditorPreferences = _loadEditorPreferences();
+		public static string PaceDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PACE");
 		public static string DefaultProjectsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "PACE Projects");
+		public static string ProjectRootFilename = "project.xml";
+		public static string EditorPreferencesFilepath = Path.Combine(PaceDataFolder, "preferences.xml");
+
 		public static string ProjectRootFromFolder(string folderPath) =>
-			Path.Combine(folderPath, ProjectRootFile);
+			Path.Combine(folderPath, ProjectRootFilename);
+
+		private static EditorPreferences _loadEditorPreferences()
+		{
+			EditorPreferences preferences;
+			if (File.Exists(EditorPreferencesFilepath))
+				preferences = GameObjectSerializer.LoadFile<EditorPreferences>(EditorPreferencesFilepath);
+			else
+			{
+				preferences = new EditorPreferences();
+				// Create preferences file in PACE app data
+				Directory.CreateDirectory(PaceDataFolder);
+				GameObjectSerializer.SaveToFile(preferences, EditorPreferencesFilepath);
+			}
+			return preferences;
+		}
 	}
 }
