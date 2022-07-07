48f5ef4b52205bb1c54236c0ce638a3b06282742
William Rågstad <william.ragstad@gmail.com>
2022-07-07 12:53:51 +0200

    Added builtin themes

--- PointAndClickEngine/EngineConfig.cs
+++ PointAndClickEngine/EngineConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PointAndClickEngine.Models;
 
 namespace PointAndClickEngine
 {
@@ -13,5 +15,17 @@ namespace PointAndClickEngine
 		public static string DefaultProjectsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "PACE Projects");
 		public static string ProjectRootFromFolder(string folderPath) =>
 			Path.Combine(folderPath, ProjectRootFile);
+
+		public static class BuiltinThemes
+		{
+			public static EditorTheme Default = new EditorTheme()
+			{
+				FormBackgroundPrimary = Color.FromKnownColor(KnownColor.Control)
+			};
+			public static EditorTheme Dark = new EditorTheme()
+			{
+				FormBackgroundPrimary = Color.FromArgb(45, 45, 45)
+			};
+		}
 	}
 }
